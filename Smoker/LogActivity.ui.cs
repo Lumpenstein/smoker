4aff6062f53e72648f181627f370c382ea4ed3d5
Lumpenstein <jhang_90@hotmail.com>
2018-03-19 20:50:08 +0100

    added Logactivity with back button

---
+++ Smoker/LogActivity.ui.cs
@@ -0,0 +1,34 @@
+﻿using Android.Widget;
+using GalaSoft.MvvmLight.Views;
+using smoker;
+
+namespace Smoker
+{
+    // In this partial Activity, we provide access to the UI elements.
+    // This file is partial to keep things cleaner in the LogActivity.cs
+    // See http://blog.galasoft.ch/posts/2014/11/structuring-your-android-activities/
+    public partial class LogActivity : ActivityBase
+    {
+        private Button _btnBack;
+        //private Button _btnBack;
+
+        public Button BtnBack
+        {
+            get
+            {
+                return _btnBack
+                       ?? (_btnBack = FindViewById<Button>(Resource.Id.log_btn_back));
+            }
+        }
+
+        //public Button BtnBack
+        //{
+        //    get
+        //    {
+        //        return _btnBack
+        //               ?? (_btnBack = FindViewById<Button>(Resource.Id.log_btn_back));
+        //    }
+        //}
+
+    }
+}
